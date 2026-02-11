1f5a9b957d5a4a5287508a933db84af560eb9e40
beanz-y <205116927+beanz-y@users.noreply.github.com>
2026-02-10 22:23:10 -0700

    web ready version V1

--- crystalize-web/vite.config.js
+++ crystalize-web/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This ensures assets are linked relatively (e.g. "./script.js" instead of "/script.js")
+  // This makes the app flexible: it will work on voronoi.site.com OR site.com/voronoi
+  base: './', 
 })
